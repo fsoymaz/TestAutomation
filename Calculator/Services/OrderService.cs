35e4d4bf70116543e0b998ad3f38f2414503f1d1
fsoymaz <fthsymz60@gmail.com>
2026-01-15 00:41:13 +0300

    Bug: Force addition instead of multiplication

--- Calculator/Services/OrderService.cs
+++ Calculator/Services/OrderService.cs
@@ -34,7 +34,8 @@ public class OrderService
         if (!updateResult)
             throw new Exception("Failed to update stock in database.");
 
-        // Toplam tutari don
-        return product.Price * quantity;
+        // Toplam tutari don (BILEREK HATA YAPIYORUZ)
+        return product.Price + quantity;
+        
     }
 }
